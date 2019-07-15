ce6befb17e87ab5075e45261f8cfba8ba6a17b6b
tangzhixin <1586782353@qq.com>
2019-07-15 14:54:08 +0800

    aaa

--- app/Http/Middleware/Update.php
+++ app/Http/Middleware/Update.php
@@ -23,9 +23,9 @@ class Update
         $time=time();
         $a=strtotime('9:00:00');
         $b=strtotime('17:00:00');
-        if($time<$a){
+        if($time<=$a){
             echo "还没到9:00,商品修改需要在【9:00-17:00】才可以进入】";die;
-        }else if($time>$b){
+        }else if($time>=$b){
             echo "已经过了17:00了，商品修改需要在【9:00-17:00】才可以进入】";die;
         }
         return $next($request);
